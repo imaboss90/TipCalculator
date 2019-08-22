357ffa6455d9b26d801158a5c0f4bdd9ddf0a056
Ryan Walentowicz <walentowicz3691@students.d211.org>
2019-08-22 13:21:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tipcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View v){
+        // Get references to all elements on app screen
+        EditText billEditText = (EditText) findViewById(R.id.billEditText);
+        EditText tipEditText = (EditText) findViewById(R.id.tipEditText);
+        TextView finalBillView = (TextView) findViewById(R.id.finalBillView);
+
+        // get the values from the EditText boxes and convert them to int data types
+        int bill = Integer.parseInt(billEditText.getText().toString());
+        int tip = Integer.parseInt(tipEditText.getText().toString());
+
+        // add the two ints
+        int perc = tip/100;
+        int sum = bill * perc;
+
+        // set the text for the answer
+        finalBillView.setText(""+sum);
+    }
+}
